2b84c93068aa9e1802b59954b474c8d761c0f7a1
Nate Brown <n.b.nrb99@icloud.com>
2024-02-25 17:07:16 -0600

    more functionality

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,38 @@
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use crossterm::{
     terminal::{size, Clear, ClearType},
     QueueableCommand,
 };
 use std::{
+    fmt::{Display, Formatter},
     io::{stdout, Result, Write},
     thread::sleep,
     time::Duration,
 };
+
+#[derive(Debug, Clone, ValueEnum)]
+enum Pattern {
+    XOR,
+    XNOR,
+}
+
+impl Pattern {
+    fn pat(&self, x: u16, t: u16) -> u16 {
+        match self {
+            Self::XOR => x ^ t,
+            Self::XNOR => x ^ !t,
+        }
+    }
+}
+
+impl Display for Pattern {
+    fn fmt(&self, fmt: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::XOR => write!(fmt, "xor"),
+            Self::XNOR => write!(fmt, "xnor"),
+        }
+    }
+}
 
 #[derive(Parser, Debug)]
 #[command(version = "0.0.0", about = "a", long_about = "b")]
@@ -20,7 +45,13 @@ struct Args {
     initialt: u16,
     /// length of frame, in ten-thousandths of a second
     #[arg(short, long, default_value_t = 4_000)]
-    frame: u64
+    frame: u64,
+    #[arg(short, long, default_value_t = false)]
+    cumulative: bool,
+    #[arg(short, long, default_value_t = Pattern::XOR)]
+    pattern: Pattern,
+    #[arg(short, long, default_value_t = '*')]
+    on: char
 }
 
 fn main() -> Result<()> {
@@ -28,7 +59,11 @@ fn main() -> Result<()> {
     let (w, h) = size()?;
     let mut t = args.initialt;
     let x0 = args.leftx;
+    let cumulative = args.cumulative;
+    let pat = args.pattern;
     let length = Duration::from_micros(100 * args.frame);
+    let mut b = [0;4];
+    args.on.encode_utf8(&mut b);
     let x1 = x0 + w;
     let mut stdout = stdout();
     let xr = x0..x1;
@@ -36,7 +71,7 @@ fn main() -> Result<()> {
     let mut yv = vec![0; w as usize];
     loop {
         for i in 0..(w as usize) {
-            yv[i] = xv[i] ^ t;
+            yv[i] = pat.pat(xv[i], t);
         }
         let maxy = yv
             .iter()
@@ -47,9 +82,13 @@ fn main() -> Result<()> {
             yv[i] = ((yv[i] as u32 * h as u32) / maxy as u32) as u16;
         }
         for y in 1..=h {
-            let y = h-y;
+            let y = h - y;
             for x in 0..w as usize {
-                stdout.write_all(if yv[x] >= y { b"0" } else { &[32] })?;
+                if cumulative {
+                    stdout.write(if yv[x] >= y { &b } else { &[32] })?;
+                } else {
+                    stdout.write(if yv[x] == y { &b } else { &[32] })?;
+                }
             }
         }
         stdout.flush()?;
